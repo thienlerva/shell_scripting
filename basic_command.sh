dc6763dd8dad3b468276babd8ad5925b13c09471
Thien <thienxuanle@yahoo.com>
2020-06-15 14:37:56 -0400

    ln -s softlink

--- basic_command.sh
+++ basic_command.sh
@@ -6,13 +6,17 @@ ls -l
 mkdir dir1
 cd dir1
 cd .. go back 1 level
+cd ../../ 2 level
 cd / go back to root dir
 cd ~ go home
 cd ~/dir1
 clear or ctr + l
 history
+chmod +x myscript.sh
 touch file1.txt -> create file
 nano file1.txt or code . or code file1.txt
+cat > file1.txt // to write a text from terminal to file (override old text). Enter -> Ctr+C
+cat >> file2.txt // append to text to tile
 cat file1.txt -> view content
 less file1.txt -> view entire file
 q to quit
@@ -23,11 +27,16 @@ rm file1.txt -> to remove file
 rmdir dir2 -> remove dir
 rm -R dir2 -> force to remove dir with file inside
 which mysql -> to view directory
+man ping or ping -h
 sudo
 ifconfig
+ifconfig eho0
 iwconfig
 ping google.com
 ctr+c
+netstat // View statistic, active network connection
+nslookup google.com  // lookup ip and reverse
+dig
 uname -a -> view system info
 blkid -> view hadrdrive
 top -> view process
@@ -37,6 +46,10 @@ sudo apt-get remove filemozilla
 sudo apt-get update
 gedit file.name
 hostnamectl   //check RHEL version
+timedatectl   // check time
+timedatectl set-local-rtc yes
+timedatectl set-time [YYYY-MM-DD]
+timedatectl set-time [HH:MM:SS]
 vim ~/.bashrc
 ccache -c  // clear cache
 ls -1 | wc -l   // count files in dir
@@ -45,6 +58,9 @@ pidof firefox
 kill id
 or kill $(pidof firefox)
 init 6   // shut down
-ln -s. // set soft link
-dv -sh Documents/   // check folder size
+ln -s origin_file_dir destination_file_dir // set soft link
+du -sh Documents/   // check folder size
 grep -rni “dry bulk cargo” *   // search file In dir
+shutdown -r  // to shutdown
+dd // create image file
+chroot // create sandbox
