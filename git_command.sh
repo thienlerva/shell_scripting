16465ff9da175fe85d4f40979a295a1cb8b16825
Thien Le <44382928+thienlerva@users.noreply.github.com>
2020-12-08 19:49:56 -0500

    Update git_command.sh

--- git_command.sh
+++ git_command.sh
@@ -4,12 +4,13 @@ git branch -m newname // change branch name
 git branch -d branchname // delete branch
 git branch -a | grep -i OIG-7778
 git checkout <filename>  // revert a file 
+git checkout .  // revert all files
 git merge origin devel  // merge develop into developer branch, stay on current branch KAMPS-555
 git cherry-pick --edit <hash> <hash> same as merge but apply "only code changes" at a specific commit
 git mergetool  // resolve merge conflict
 git mergetool --tool-help
   // merge fail
-git reset --hard HEAD~1
+git reset --hard HEAD~1 or HEAD~2 reset commit in the working tree after commit but before push.
 git stash
 git stash list
 git stash apply
@@ -19,7 +20,7 @@ git commit --amend  // amend last commit message
 git checkout <commit_id> path/to/file.txt
 git checkout adfdgdggdf~1 text.txt  // Revert commit before 1
  -- Revert changes introduced by a specific commit
-git revert <commit_id>
+git revert <commit_id> revert changes in the remote repo after push.
 git ls --author "Thien Le"
 gitk  //show changes
 git show <commit_hash>
